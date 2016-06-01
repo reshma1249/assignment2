b7cb7a36c850d34c845e3f6c63d5004247261d00
reshma1249 <res_ma91@rediffmail.com>
2016-06-01 12:07:04 -0500

    Add files via upload

---
+++ Reshma_assignment2/Reshma.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Reshma_assignment2
+{
+    partial class Reshma
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuItemClose = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuItemAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(640, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuItemClose});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // menuItemClose
+            // 
+            this.menuItemClose.Name = "menuItemClose";
+            this.menuItemClose.Size = new System.Drawing.Size(92, 22);
+            this.menuItemClose.Text = "Exit";
+            this.menuItemClose.Click += new System.EventHandler(this.menuItemClose_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuItemAbout});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // menuItemAbout
+            // 
+            this.menuItemAbout.Name = "menuItemAbout";
+            this.menuItemAbout.Size = new System.Drawing.Size(111, 22);
+            this.menuItemAbout.Text = "Author";
+            this.menuItemAbout.Click += new System.EventHandler(this.menuItemAbout_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Text to process:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 53);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(484, 202);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(221, 265);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Count words";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 315);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(484, 122);
+            this.textBox2.TabIndex = 4;
+            // 
+            // Reshma
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 449);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Reshma";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Words for friends";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolStripMenuItem menuItemClose;
+        private System.Windows.Forms.ToolStripMenuItem menuItemAbout;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Reshma_assignment2/Reshma.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Reshma_assignment2
+{
+    public partial class Reshma : Form
+    {
+        public Reshma()
+        {
+            InitializeComponent();
+        }
+        //REFERENCE: http://nlpdotnet.com/SampleCode/ListWordsByFrequency.aspx
+
+        private static char[] delimiters_no_digits = new char[] {
+            '{', '}', '(', ')', '[', ']', '>', '<','-', '_', '=', '+',
+      '{', '}', '(', ')', '[', ']', '>', '<','-', '_', '=', '+',
+            '|', '\\', ':', ';', ' ', ',', '.', '/', '?', '~', '!',
+            '@', '#', '$', '%', '^', '&', '*', ' ', '\r', '\n', '\t',
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+        private string[] Tokenize(string text)
+        {
+            string[] tokens = text.Split(delimiters_no_digits, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < tokens.Length; i++)
+            {
+                string token = tokens[i];
+
+                // Change token only when it starts and/or ends with "'" and  
+                // it has at least 2 characters. 
+
+                if (token.Length > 1)
+                {
+                    if (token.StartsWith("'") && token.EndsWith("'"))
+                        tokens[i] = token.Substring(1, token.Length - 2); // remove the starting and ending "'" 
+
+                    else if (token.StartsWith("'"))
+                        tokens[i] = token.Substring(1); // remove the starting "'" 
+
+                    else if (token.EndsWith("'"))
+                        tokens[i] = token.Substring(0, token.Length - 1); // remove the last "'" 
+                }
+            }
+
+            return tokens;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuItemClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void menuItemAbout_Click(object sender, EventArgs e)
+        {
+            Modal authorForm = new Modal();
+            authorForm.ShowDialog();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            this.Text = textBox1.Text;
+            
+        }
+        private Dictionary<string, int> ToStrIntDict(string[] words)
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+
+            foreach (string word in words)
+            {
+                // if the word is in the dictionary, increment its freq. 
+                if (dict.ContainsKey(word))
+                {
+                    dict[word]++;
+                }
+                // if not, add it to the dictionary and set its freq = 1 
+                else
+                {
+                    dict.Add(word, 1);
+                }
+            }
+
+            return dict;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != string.Empty)
+            {
+                string[] words = Tokenize(textBox1.Text);
+
+                if (words.Length > 0)
+                {
+                    Dictionary<string, int> dict = ToStrIntDict(words);
+                    // SortOrder sortOrder = textBox1.Checked ? SortOrder.Ascending;
+                    //dict = Reshma_assignment2( dict, sortOrder );
+                    StringBuilder resultSb = new StringBuilder(dict.Count * 9);
+                    foreach (KeyValuePair<string, int> entry in dict)
+                        resultSb.AppendLine(string.Format("{0} [{1}]", entry.Key, entry.Value));
+                    textBox2.Text = resultSb.ToString();
+
+                }
+            }
+        }
+
+       
+    }
+}
